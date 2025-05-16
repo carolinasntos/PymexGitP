8ccd7246d079e8d8839b48c44b10048dd4566eb4
carolinasntos <a01174992@tec.mx>
2025-05-15 18:50:31 -0600

    <3

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await axios.post("http://localhost:3001/login", {
+      const response = await axios.post("https://railwayback-production-781f.up.railway.app/login", {
         correo: correo,
         hashContrasena: contrasena,
       });
